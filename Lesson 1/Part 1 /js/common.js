a8ba82dff27fbecd73eea4fcf136a90965a3f9fc
ArtemChernii <artemcherniy7@gmail.com>
2018-03-27 20:50:31 +0100

    My not very first commit:)

---
+++ Lesson 1/Part 1 /js/common.js
@@ -0,0 +1,18 @@
+	var budget   = prompt("Ваш бюджет?", "100 рублей");
+		  shopName = prompt("Название вашего магазина?", "Aldi");
+		  mainList = {
+		budget: 0,
+		shopName: " ",
+		shopGoods: [],
+		employers: {
+			employerName: []
+		},
+		open: true
+	};
+
+	mainList.shopGoods[0] = prompt("Какой тип товара будем продавать?");
+	mainList.shopGoods[1] = prompt("Какой тип товара будем продавать?");
+	mainList.shopGoods[2] = prompt("Какой тип товара будем продавать?");
+
+	alert(+budget/30 + " рублей");
+	console.log(mainList.shopGoods);
---
+++ Lesson 1/Part 2/js/common.js
@@ -0,0 +1,9 @@
+	var num = "33721";
+
+	multi = num[0]*num[1]*num[2]*num[3]*num[4];
+	
+	console.log(multi);
+	// result = Math.pow(multi, 3);
+	result = multi**3;
+	alert(result)
+	console.log(result);
